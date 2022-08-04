261c4b58bf177c59960814388964f3f09cfcb2c5
mubarek78 <shifamubarek@yahoo.com>
2022-08-03 18:06:33 -0600

    assigment complited

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ gem "activerecord", '~> 6.1'
 gem "json", "~> 1.8", ">= 1.8.5"
 gem "pry"
 gem "rake"
-gem "sqlite3", "~> 1.4"
+gem "sqlite3", "~> 1.4.2"
 
 group :test do
   gem "database_cleaner"
--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,19 @@ require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 require 'active_record'
 
-require_relative '../lib/dog'
 
 ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "./db/test.sqlite3"
 )
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+
+require_relative '../lib/dog'
